868a7d55ed20b6c22062dac710ce901fafba988c
mammo0 <marc.ammon@hotmail.de>
2020-02-18 15:57:15 +0100

    added steps for program initialization

--- NetworkMiner/Program.cs
+++ NetworkMiner/Program.cs
@@ -6,12 +6,16 @@
 
 using SharedUtils;
 using System;
+using System.IO;
 using System.Reflection;
 using System.Diagnostics;
 using System.Globalization;
 
 namespace NetworkMiner {
     public static class Program {
+
+        private static string EXE_PATH = Path.GetFullPath(Assembly.GetEntryAssembly().Location);
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -19,8 +23,25 @@ namespace NetworkMiner {
             // first set up the logger
             SetupLogger("NetworkMiner");
 
+            // get the pcap file from the CLI arguments
+            string[] parms = Environment.GetCommandLineArgs();
+            string filename = null;
+            if (parms.Length > 1) {
+                filename = parms[parms.Length - 1];
+                if (! File.Exists(filename)) {
+                    PrintError("The specified file '" + filename + "' doesn't exist!", 2);
+                }
+            } else {
+                PrintError("No PCAP file specified!", 1);
+            }
+
+            // create 'AssembledFiles/cache" directory if it doesn't exist
+            // this directory is required!
+            string basePath = Path.GetDirectoryName(EXE_PATH);
+            Directory.CreateDirectory(Path.Combine(basePath, FileStreamAssembler.ASSMEBLED_FILES_DIRECTORY, "cache"));
 
         }
+
 
         public static void SetupLogger(string applicationName) {
 
@@ -49,16 +70,21 @@ namespace NetworkMiner {
             Logger.LogToFile = true;
 #endif
 
-            String executablePath = Assembly.GetEntryAssembly().Location;
-            FileVersionInfo productInfo = FileVersionInfo.GetVersionInfo(executablePath);
+            FileVersionInfo productInfo = FileVersionInfo.GetVersionInfo(EXE_PATH);
             Logger.Log("Environment.Is64BitOperatingSystem = " + Environment.Is64BitOperatingSystem.ToString(), Logger.EventLogEntryType.Information);
             Logger.Log("Environment.Is64BitProcess = " + Environment.Is64BitProcess.ToString(), Logger.EventLogEntryType.Information);
             Logger.Log(productInfo.ProductName + " " + productInfo.ProductVersion, Logger.EventLogEntryType.Information);
-            Logger.Log("Application.ExecutablePath = " + executablePath, Logger.EventLogEntryType.Information);
+            Logger.Log("Application.ExecutablePath = " + EXE_PATH, Logger.EventLogEntryType.Information);
             Logger.Log("Application.CurrentCulture = " + CultureInfo.CurrentCulture, Logger.EventLogEntryType.Information);
             Logger.Log("Environment.Version = " + Environment.Version, Logger.EventLogEntryType.Information);//4.0.30319.42000 =  .NET Framework 4.6, its point releases, and the .NET Framework 4.7
-            Logger.Log("Setting up application rendering", Logger.EventLogEntryType.Information);
+            Logger.Log("Starting application", Logger.EventLogEntryType.Information);
 
+        }
+
+        private static void PrintError(string errorMsg, int exitCode = 1) {
+            Logger.Log(errorMsg, Logger.EventLogEntryType.Error);
+            Console.WriteLine(errorMsg);
+            System.Environment.Exit(1);
         }
     }
 }
