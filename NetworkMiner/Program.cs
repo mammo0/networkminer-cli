4e7cf69d9d76a1670a5fe5cabbd09dac0ff593b0
mammo0 <marc.ammon@hotmail.de>
2022-10-18 12:13:23 +0200

    apply cli code

--- NetworkMiner/Program.cs
+++ NetworkMiner/Program.cs
@@ -34,26 +34,21 @@ namespace NetworkMiner {
                 filename = parms[parms.Length - 1];
                 if (! File.Exists(filename)) {
                     PrintError("The specified file '" + filename + "' doesn't exist!", 2);
+                } else if (Directory.Exists(filename)) {
+                    PrintError("NetworkMiner can't open directories, try a PCAP file instead!", 3);
                 }
             } else {
                 PrintError("No PCAP file specified!", 1);
             }
 
-            // create 'AssembledFiles/cache" directory if it doesn't exist
-            // this directory is required!
-            string basePath = Path.GetDirectoryName(EXE_PATH);
-            string cacheDirectory = Path.Combine(basePath, FileStreamAssembler.ASSMEBLED_FILES_DIRECTORY, "cache");
-            Directory.CreateDirectory(cacheDirectory);
+            // create the packet handler
+            PacketHandler packetHandler = CreatePacketHandler(new DirectoryInfo(Path.GetDirectoryName(EXE_PATH)));
 
+            // parse the PCAP file
+            ParsePCAPFile(filename, packetHandler);
 
-            ParsePCAPFile(filename);
-
-
-            // remove the cache directory again
-            await FileUtils.DeleteDirectory(cacheDirectory);
-
-            // don't know why the application doesn't exit automatically after reaching the end of the main method...
-            System.Environment.Exit(0);
+            // close packet handler
+            ClosePacketHandler(packetHandler);
         }
 
 
@@ -101,20 +96,22 @@ namespace NetworkMiner {
             System.Environment.Exit(exitCode);
         }
 
-        private static void ParsePCAPFile(String filePath) {
+        private static void ParsePCAPFile(String filePath, PacketHandler packetHandler) {
             int percentRead = 0;
 
-            using (PcapFileReader pcapReader = new PcapFileReader(filePath)) {
+            Logger.Log("Loading " + filePath, Logger.EventLogEntryType.Information);
+
+            PcapFileReader pcapReader = null;
+            try {
+                using (pcapReader = new PcapFileReader(filePath)) {
                 DateTime parsingStartTime = DateTime.Now;
                 Logger.Log(filePath + " start parsing " + parsingStartTime.ToString(), Logger.EventLogEntryType.Information);
                 Console.WriteLine("Start parsing " + filePath);
 
                 int enqueuedFramesSinceLastWait = 0;
 
-                PacketHandler packetHandler = new PacketHandler(EXE_PATH, System.Environment.CurrentDirectory, null, true, new Func<DateTime, string>((DateTime dateTime) => { return dateTime.ToUniversalTime().ToString("u"); }), false);
-                packetHandler.StartBackgroundThreads();
-
                 foreach (PcapFrame pcapPacket in pcapReader.PacketEnumerator()) {
+                        try {
                     Frame frame = packetHandler.GetFrame(pcapPacket.Timestamp, pcapPacket.Data, pcapPacket.DataLinkType);
                     packetHandler.AddFrameToFrameParsingQueue(frame);
                     enqueuedFramesSinceLastWait++;
@@ -124,6 +121,12 @@ namespace NetworkMiner {
 
                         // output percent
                         Console.WriteLine("Progress: " + percentRead + "%");
+                            }
+                        } catch (Exception frameException) {
+                            Logger.Log(frameException.GetType().ToString() + " when reading frame: " + frameException.Message, Logger.EventLogEntryType.Error);
+#if DEBUG
+                            throw frameException;
+#endif
                     }
                 }
                 Logger.Log(enqueuedFramesSinceLastWait + " frames read in " + DateTime.Now.Subtract(parsingStartTime).ToString(), Logger.EventLogEntryType.Information);
@@ -142,6 +145,93 @@ namespace NetworkMiner {
                 TimeSpan parsingTimeTotal = DateTime.Now.Subtract(parsingStartTime);
                 Logger.Log(filePath + " parsed in " + parsingTimeTotal.ToString(), Logger.EventLogEntryType.Information);
                 Console.WriteLine("Finished parsing " + filePath);
+                }
+            } catch (InvalidDataException ex) {
+                    Logger.Log("LoadPcapFile1: " + ex.Message, SharedUtils.Logger.EventLogEntryType.Warning);
+
+                    if (ex.Message.Contains("Magic number is A0D0D0A"))
+                        PrintError("This is a PcapNg file. NetworkMiner Professional is required to parse PcapNg files.\n\nNetworkMiner Professional can be purchased from Netresec's website:\nhttp://www.netresec.com/", 4);
+                    else
+                        PrintError("Error opening PCAP file: " + ex.Message, 4);
+                    if (pcapReader != null)
+                        pcapReader.Dispose();
+            } catch (UnauthorizedAccessException ex) {
+                SharedUtils.Logger.Log("LoadPcapFile2: " + ex.Message, SharedUtils.Logger.EventLogEntryType.Warning);
+
+                PrintError("Unauthorized to open file " + filePath, 5);
+
+                if (pcapReader != null)
+                    pcapReader.Dispose();
+            } catch (Exception ex) {
+                SharedUtils.Logger.Log("LoadPcapFile3: " + ex.Message, SharedUtils.Logger.EventLogEntryType.Warning);
+
+                PrintError("Error opening PCAP file: " + ex.Message, 1);
+
+                if (pcapReader != null)
+                    pcapReader.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Funtion used to create a new PacketHandler
+        /// </summary>
+        /// <param name="outputDirectory"></param>
+        private static PacketHandler CreatePacketHandler(DirectoryInfo outputDirectory) {
+            //make sure that folders exists
+            try {
+                // directory for the assembled files
+                DirectoryInfo di = new DirectoryInfo(outputDirectory.FullName + Path.DirectorySeparatorChar + PacketParser.FileTransfer.FileStreamAssembler.ASSMEBLED_FILES_DIRECTORY);
+                if (!di.Exists)
+                    di.Create();
+
+                // cache directory
+                di = new DirectoryInfo(outputDirectory.FullName + Path.DirectorySeparatorChar + PacketParser.FileTransfer.FileStreamAssembler.ASSMEBLED_FILES_DIRECTORY + Path.DirectorySeparatorChar + "cache");
+                if (!di.Exists)
+                    di.Create();
+            }
+            catch (UnauthorizedAccessException ex) {
+                Logger.Log(ex.Message, SharedUtils.Logger.EventLogEntryType.Error);
+                PrintError("Please ensure that the user has write permissions in the AssembledFiles and Captures directories.", 1);
+            }
+
+            PacketHandler packetHandler = new PacketHandler(
+                // applicationExecutablePath
+                EXE_PATH,
+                // outputPath
+                outputDirectory.FullName,
+                // preloadedFingerprints
+                // NetworkMinerForm.cs -> default null
+                null,
+                // ignoreMissingFingerprintFiles
+                // PacketHandlerWrapper.cs -> always false
+                false,
+                // toCustomTimeZoneStringFunction
+                // GuiProperties.cs -> simplified
+                new Func<DateTime, string>((DateTime dateTime) => { return dateTime.ToUniversalTime().ToString("u"); }),
+                // useRelativePathIfAvailable
+                // NetworkMinerForm.cs -> default true
+                true,
+                // verifyX509Certificates
+                // NetworkMinerForm.cs -> default false
+                false);
+            packetHandler.StartBackgroundThreads();
+
+            return packetHandler;
+        }
+
+        /// <summary>
+        /// Funtion used to close an existing PacketHandler
+        /// </summary>
+        private static void ClosePacketHandler(PacketHandler packetHandler) {
+            // avoid exceptions
+            if (packetHandler != null) {
+                // stop the background threads
+                packetHandler.AbortBackgroundThreads();
+
+                // remove the cache directory
+                DirectoryInfo di = new DirectoryInfo(packetHandler.OutputDirectory + Path.DirectorySeparatorChar + PacketParser.FileTransfer.FileStreamAssembler.ASSMEBLED_FILES_DIRECTORY + Path.DirectorySeparatorChar + "cache");
+                if (di.Exists)
+                    di.Delete();
             }
         }
     }
